3226cf0cabdbb4d788e08655a2eb27ce2c65077e
stone <dh357399185@gmail.com>
2019-10-15 19:12:41 +0700

    Initial Commit

---
+++ team_city/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  team_city
+//
+//  Created by Stone on 2019/10/15.
+//  Copyright © 2019 WG. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
